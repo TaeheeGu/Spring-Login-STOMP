f1b8bbb168ad691f40a99b85a0738f61bf60d451
조인혁 <alzpqm@naver.com>
2022-03-17 19:12:49 +0900

    feat: initalize project

---
+++ src/main/java/com/fireprohibition/CBomb/CBombApplication.java
@@ -0,0 +1,13 @@
+package com.fireprohibition.CBomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CBombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CBombApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fireprohibition/CBomb/CBombApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fireprohibition.CBomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CBombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
